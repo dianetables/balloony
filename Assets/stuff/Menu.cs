939dbf7e9cacc9d90f5800b6cc92c5fb0c32b7be
dianetables <118228073+dianetables@users.noreply.github.com>
2022-11-17 01:04:20 -0800

    something sure happened

--- Assets/stuff/Menu.cs
+++ Assets/stuff/Menu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// idk if i should change the menu name o.o
 public class Menu : MonoBehaviour
 {
     //string sceneName = "";
